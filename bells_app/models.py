0caeaec503a60a9c3da6ce478dc3ca8a0953ae07
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-03 19:44:06 +0100

    created admission model, course model

--- bells_app/models.py
+++ bells_app/models.py
@@ -8,3 +8,20 @@ class Contact(models.Model):
 
 	def __str__(self):
 		return f'{self.name}, {self.email}'
+
+class Course(models.Model):
+	name = models.CharField(max_length=400)
+	cut_off_mark = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
+
+class Admission_List(models.Model):
+	name = models.CharField(max_length=300, blank=False, null=False)
+	email = models.EmailField(max_length=300, blank=False, null=False)
+	phone = models.CharField(max_length=300)
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
--- bells_app/views.py
+++ bells_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact
 
 # Create your views here.
 def index(request):
@@ -9,4 +10,12 @@ def admission_page(request):
 	return render(request, 'admission_page.html', {})
 
 def contact_page(request):
+	if request.method == "POST":
+		name = request.POST['name']
+		email = request.POST['email']
+		message = request.POST['message']
+		new_contact = Contact.objects.create(name=name, email=email, message=message)
+		new_contact.save()
+		print("saved")
+		return render(request, 'contact_page.html', {})
 	return render(request, 'contact_page.html', {})
