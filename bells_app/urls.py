68067bdc70dd319de3cdd28cd7f7fc0fa8765121
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-03 13:27:33 +0100

    contact page, admission page

--- bells_app/urls.py
+++ bells_app/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admission_page', views.admission_page, name='admission_page'),
+    path('contact_page', views.contact_page, name='contact_page'),
 ]
--- bells_app/views.py
+++ bells_app/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
 	return render(request, 'index.html', {})
+
+
+def admission_page(request):
+	return render(request, 'admission_page.html', {})
+
+def contact_page(request):
+	return render(request, 'contact_page.html', {})
